11cb568a3ad8bd648a73c9dbf527b900152b0c10
biswajeet-g <ganthiabiswa@gmail.com>
2026-02-07 12:46:45 +0530

    bubble sort

---
+++ JAVA/BubbleSort.java
@@ -0,0 +1,30 @@
+class BubbleSort {
+    void bubbleSort(int[] arr){
+        int n=arr.length;
+
+        for (int i=0;i<n-1;i++){
+            boolean swapped = false;
+
+            for(int j=0; j<n-1-i;j++){
+                if (arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+
+                    swapped=true;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] arg){
+        BubbleSort b =new BubbleSort();
+        int[] arr = {10,3,14,15,1,6,23,21};
+        b.bubbleSort(arr);
+
+        System.out.println("Sorted array:");
+        for (int x : arr) {
+            System.out.print(x + " ");
+        }
+    }
+}
